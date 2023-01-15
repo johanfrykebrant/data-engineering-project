af72346668852150cd811211d73f7d624009591c
Johan Frykebrant <85884666+johanfrykebrant@users.noreply.github.com>
2023-01-15 15:39:08 +0100

    temp sensor

--- temperature-sensor/temp-sens-producer.py
+++ temperature-sensor/temp-sens-producer.py
@@ -1,9 +1,19 @@
-from csv import reader
 import os
 import glob
 import time
+import socket
 from datetime import datetime
+import logging 
+import json
+from dotenv import load_dotenv
+from kafka import KafkaProducer
 
+logging.basicConfig(filename="std.log", 
+                    format='%(asctime)s | %(message)s', 
+                    filemode='w') 
+
+logger=logging.getLogger() 
+logger.setLevel(logging.DEBUG) 
 
 os.system('sudo modprobe w1-gpio')
 os.system('sudo modprobe w1-therm')
@@ -22,44 +32,54 @@ def find_sensors():
 def read_sens_lines():
     lines = []
     device_files = find_sensors()
+    
     for i in range(len(device_files)):
         f = open(device_files[i], 'r')
         lines.append(f.readlines())
         f.close()
+    
     return lines
     
 def read_temp():
-    result_dict = {"Sensor values" : []}
+    result_dict = {
+                    'destination_schema': 'staging',
+                    'destination_table': 'observations',
+                    'data': []
+    }
     lines = read_sens_lines()
-    for i in range(len(lines)):
-        line = lines[i]
-        s = ''.join( [n for n in SENSORS[i] if n.isdigit()] )
-        temp_dict ={ 
-            "name" : "Temperature sensor #" +s[-3:],
-            "timestamp" : None,
-            "value" : None,
-            "unit" : "degC"
-            }
-        
-        #Oklart vad jag håller på med här... :)
+
+    for line in lines:
         while line[0].strip()[-3:] != 'YES':
-            time.sleep(0.2)
-            lines = read_sens_lines()
-        
-        equals_pos = line[1].find('t=')
+                time.sleep(0.2)
+                lines = read_sens_lines()
+
+    for i, line in lines:     
+        equals_pos = line.find('t=')
         if equals_pos != -1:
-            temp_dict["value"] = float(line[1][equals_pos+2:]) / 1000.0
+            value = float(line[equals_pos+2:]) / 1000.0
+
+            temp_dict = {
+                    'observation_name':'Lufttemperatur',
+                    'observation_timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    'observation_value': value,
+                    'observation_station': socket.gethostname(),
+                    'observation_unit': 'degC'
+                }
+            result_dict["data"].append(temp_dict)  
         else:
             raise Exception("Sensors found but unable to read value.")
-
-        temp_dict["timestamp"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        result_dict["Sensor values"].append(temp_dict)    
-        
+       
     return result_dict
 
 def main():
-    t = read_temp()
-    print(t)
+    load_dotenv()
+    producer = KafkaProducer(bootstrap_servers=f"{os.getenv('KAFKA_IP')}:{os.getenv('KAFKA_PORT')}")
+    msg = read_temp()
+    msg_byte = json.dumps(msg).encode('utf-8')
+    # send message to kafka topic
+    logger.debug(f"{datetime.now()} - Send temperature measurements to db-ingestion topic...")
+    producer.send('db-ingestion', msg_byte)
+    print(json.dumps(msg, indent=4))
     
 if __name__ == "__main__":
     main()
