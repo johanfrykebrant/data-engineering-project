d99f6c2a3e3f27715cf8c910e2c194702d892c61
Johan Frykebrant <85884666+johanfrykebrant@users.noreply.github.com>
2023-01-18 17:51:15 +0100

    forecast increment dbt

--- dbt_proj/models/forecasts.sql
+++ dbt_proj/models/forecasts.sql
@@ -1,10 +1,23 @@
-{{ config(materialized='table',schema='analytics') }}
+{{
+    config(
+        materialized='incremental'
+		, schema='analytics'
+    )
+}}
 
-SELECT name AS forecast_name,
-	forecast_timestamp,	
-	CAST(EXTRACT(epoch FROM (forecast_timestamp - forecast_approved_timestamp)/(60*60)) AS INT) AS hours_diff,
-	forecast_value,
-	forecast_unit
+SELECT 	name AS forecast_name,
+		forecast_timestamp,	
+		CAST(EXTRACT(epoch FROM (forecast_timestamp - forecast_approved_timestamp)/(60*60)) AS INT) AS hours_diff,
+		forecast_value,
+		forecast_unit,
+		created_timestamp_utc ,
+		NOW() AS dbt_timestamp_utc 
 FROM staging.forecasts
 	JOIN staging.forecast_name_codes
 	ON forecast_code = code
+
+
+
+{% if is_incremental() %}
+WHERE created_timestamp_utc > (SELECT MAX(created_timestamp_utc) FROM {{ this }})
+{% endif %}
